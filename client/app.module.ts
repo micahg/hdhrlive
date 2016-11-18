4c9f2732f02266d8178a571af4f961575d402f80
Micah Galizia <micahgalizia@gmail.com>
2016-11-17 22:16:09 -0500

    bringing in bootstrap and trying to fix compiler dependencies

--- client/app.module.ts
+++ client/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpModule } from "@angular/http";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppComponent } from "./app.component";
 import { HDHRService } from "./hdhr.service";
 
 @NgModule({
-  imports: [ HttpModule, BrowserModule ],
+  imports: [ HttpModule, BrowserModule, NgbModule.forRoot() ],
   declarations: [ AppComponent ],
   providers: [ HDHRService],
   bootstrap: [ AppComponent ]
--- client/public/systemjs.config.js
+++ client/public/systemjs.config.js
@@ -24,6 +24,8 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      // bootstrap
+      '@ng-bootstrap/ng-bootstrap': 'node_modules/@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.js',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
